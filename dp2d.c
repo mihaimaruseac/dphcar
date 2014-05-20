13d24726bbb3ea75c40dc3a939c0f465939c6ae4
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-05-20 11:52:19 -0400

    Add test experiments and remove separation coefficient.

--- dp2d.c
+++ dp2d.c
@@ -16,8 +16,6 @@ struct experiment {
 	float c;
 	/* exponent */
 	float k;
-	/* separation */
-	int K;
 	/* Z values */
 	mpfr_t Z0, Z;
 };
@@ -25,12 +23,17 @@ struct experiment {
 /**
  * Constant values used to generate experiments and to run them.
  */
+#if 0 /* production */
 float ks[] = {1, 1.2, 1.4, 1.6, 1.8, 2, 2.0, 2.2, 2.4, 2.6, 2.8, 3};
 int ks_sz = sizeof(ks) / sizeof(ks[0]);
-float cs[] = {.1, .2, .25, .3, .4, .5, .6, .7, .75};
+float cs[] = {.1, .2, .25, .3, .4, .5, .6, .7, .75, .8, .9};
 int cs_sz = sizeof(cs) / sizeof(cs[0]);
-int Ks[] = {1, 10, 100};
-int Ks_sz = sizeof(Ks) / sizeof(Ks[0]);
+#else /* test */
+float ks[] = {1, 1.2};
+int ks_sz = sizeof(ks) / sizeof(ks[0]);
+float cs[] = {.7, .8, .9};
+int cs_sz = sizeof(cs) / sizeof(cs[0]);
+#endif
 
 #define ROUND_MODE MPFR_RNDN
 
@@ -38,83 +41,81 @@ int Ks_sz = sizeof(Ks) / sizeof(Ks[0]);
 #define PPC 5
 static struct experiment *prepare_experiments(int t, int minsup, int *exps_sz)
 {
-	int sz = ks_sz * Ks_sz * (cs_sz * PPC + EXTRA), eix = 0, i, j, k;
+	int sz = ks_sz * (cs_sz * PPC + EXTRA), eix = 0, i, k;
 	struct experiment *exps = calloc(sz, sizeof(exps[0]));
 
 	/* extra experiments first */
+	for (i = 0; i < ks_sz; i++) {
+		/* upper corner */
+		exps[eix].x = t; exps[eix].y = t;
+		exps[eix].c = 0; exps[eix].k = ks[i];
+		mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
+		mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
+		eix++;
+		/* lower corner */
+		exps[eix].x = t; exps[eix].y = 0;
+		exps[eix].c = 0; exps[eix].k = ks[i];
+		mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
+		mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
+		eix++;
+		/* line bisector meeting point */
+		exps[eix].x = t/2; exps[eix].y = t/2;
+		exps[eix].c = 0; exps[eix].k = ks[i];
+		mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
+		mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
+		eix++;
+		/* median meeting point */
+		exps[eix].x = 2*t/3; exps[eix].y = t/3;
+		exps[eix].c = 0; exps[eix].k = ks[i];
+		mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
+		mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
+		eix++;
+		/* in between (t,t) and (minsup, minsup) */
+		exps[eix].x = (t+minsup)/2; exps[eix].y = (t+minsup)/2;
+		exps[eix].c = 0; exps[eix].k = ks[i];
+		mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
+		mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
+		eix++;
+	}
+
+	/* sanity check */
+	if (eix != EXTRA * ks_sz)
+		die("Error in code, %d %d\n", eix, EXTRA * ks_sz);
+
+	/* confidence-based experiments */
 	for (i = 0; i < ks_sz; i++)
-		for (j = 0; j < Ks_sz; j++) {
-			/* upper corner */
-			exps[eix].x = t; exps[eix].y = t;
-			exps[eix].c = 0; exps[eix].k = ks[i]; exps[eix].K = Ks[j];
+		for (k = 0; k < cs_sz; k++) {
+			/* max */
+			exps[eix].x = t; exps[eix].y = t * cs[k];
+			exps[eix].c = cs[k]; exps[eix].k = ks[i];
 			mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
 			mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
 			eix++;
-			/* lower corner */
-			exps[eix].x = t; exps[eix].y = 0;
-			exps[eix].c = 0; exps[eix].k = ks[i]; exps[eix].K = Ks[j];
-			mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
-			mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
-			eix++;
-			/* line bisector meeting point */
-			exps[eix].x = t/2; exps[eix].y = t/2;
-			exps[eix].c = 0; exps[eix].k = ks[i]; exps[eix].K = Ks[j];
-			mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
-			mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
-			eix++;
-			/* median meeting point */
-			exps[eix].x = 2*t/3; exps[eix].y = t/3;
-			exps[eix].c = 0; exps[eix].k = ks[i]; exps[eix].K = Ks[j];
+			/* mid */
+			exps[eix].x = t/2; exps[eix].y = t * cs[k] / 2;
+			exps[eix].c = cs[k]; exps[eix].k = ks[i];
 			mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
 			mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
 			eix++;
 			/* in between (t,t) and (minsup, minsup) */
-			exps[eix].x = (t+minsup)/2; exps[eix].y = (t+minsup)/2;
-			exps[eix].c = 0; exps[eix].k = ks[i]; exps[eix].K = Ks[j];
+			exps[eix].x = (t+minsup)/2; exps[eix].y = (t+minsup) * cs[k]/2;
+			exps[eix].c = cs[k]; exps[eix].k = ks[i];
+			mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
+			mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
+			eix++;
+			/* y=minsup */
+			exps[eix].x = minsup/cs[k]; exps[eix].y = minsup;
+			exps[eix].c = cs[k]; exps[eix].k = ks[i];
+			mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
+			mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
+			eix++;
+			/* mid in valid region */
+			exps[eix].x = (t + minsup / cs[k])/2; exps[eix].y = (t*cs[k] + minsup)/2;
+			exps[eix].c = cs[k]; exps[eix].k = ks[i];
 			mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
 			mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
 			eix++;
 		}
-
-	/* sanity check */
-	if (eix != EXTRA * ks_sz * Ks_sz)
-		die("Error in code, %d %d\n", eix, EXTRA * ks_sz * Ks_sz);
-
-	/* confidence-based experiments */
-	for (i = 0; i < ks_sz; i++)
-		for (j = 0; j < Ks_sz; j++)
-			for (k = 0; k < cs_sz; k++) {
-				/* max */
-				exps[eix].x = t; exps[eix].y = t * cs[k];
-				exps[eix].c = cs[k]; exps[eix].k = ks[i]; exps[eix].K = Ks[j];
-				mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
-				mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
-				eix++;
-				/* mid */
-				exps[eix].x = t/2; exps[eix].y = t * cs[k] / 2;
-				exps[eix].c = cs[k]; exps[eix].k = ks[i]; exps[eix].K = Ks[j];
-				mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
-				mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
-				eix++;
-				/* in between (t,t) and (minsup, minsup) */
-				exps[eix].x = (t+minsup)/2; exps[eix].y = (t+minsup) * cs[k]/2;
-				exps[eix].c = cs[k]; exps[eix].k = ks[i]; exps[eix].K = Ks[j];
-				mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
-				mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
-				eix++;
-				/* y=minsup */
-				exps[eix].x = minsup/cs[k]; exps[eix].y = minsup;
-				exps[eix].c = cs[k]; exps[eix].k = ks[i]; exps[eix].K = Ks[j];
-				mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
-				mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
-				eix++;
-				/* mid in valid region */
-				exps[eix].x = (t + minsup / cs[k])/2; exps[eix].y = (t*cs[k] + minsup)/2;
-				exps[eix].c = cs[k]; exps[eix].k = ks[i]; exps[eix].K = Ks[j];
-				mpfr_init_set_ui(exps[eix].Z0, 0, ROUND_MODE);
-				mpfr_init_set_ui(exps[eix].Z, 0, ROUND_MODE);
-				eix++;
-			}
 
 	*exps_sz = eix;
 	return exps;
@@ -144,7 +145,7 @@ static void free_experiments(struct experiment *exps, int exps_sz)
 
 static void print_experiment(struct experiment *xp)
 {
-	printf("(%d, %d) %4.2f%%, ^%4.2f, |%d Z0=", xp->x, xp->y, xp->c, xp->k, xp->K);
+	printf("(%d, %d) %4.2f%%, ^%4.2f, | Z0=", xp->x, xp->y, xp->c, xp->k);
 	mpfr_out_str(stdout, 10, 0, xp->Z0, ROUND_MODE);
 	printf(" Z=");
 	mpfr_out_str(stdout, 10, 0, xp->Z, ROUND_MODE);
