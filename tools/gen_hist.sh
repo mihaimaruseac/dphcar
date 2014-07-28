eb7d01295310582d236a16aaeb5d8047cfe02315
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-07-28 16:17:28 -0400

    Uniq.

--- tools/gen_hist.sh
+++ tools/gen_hist.sh
@@ -6,7 +6,7 @@ process() {
     echo
     echo "Histogram:"
     for d in `seq 9 -1 0`; do
-        echo '>0.'${d}00000: `grep -e '->' ${file} | grep "0\.$d[^ ]*\$" | wc -l`;
+        echo '>0.'${d}00000: `grep -e '->' ${file} | sort | uniq | grep "0\.$d[^ ]*\$" | wc -l`;
     done
 }
 
