b6cac5510c69f2f91c62b85576803394046377c5
Mihai Maruseac <mihai.maruseac@gmail.com>
2016-08-10 13:36:10 -0400

    Solve warning and properly format item table

--- dp2d.c
+++ dp2d.c
@@ -434,7 +434,7 @@ static inline void print_item_table(const struct item_count *ic, size_t n)
 
 	printf("\n");
 	for (i = 0; i < n; i++)
-		printf("%5d[%5.2lf] %5d %7d %9.2lf\n", i, (i + 1.0)/n,
+		printf("%5lu[%5.2lf] %5d %7d %9.2lf\n", i, (i + 1.0)/n,
 				ic[i].value, ic[i].real_count,
 				ic[i].noisy_count);
 }
