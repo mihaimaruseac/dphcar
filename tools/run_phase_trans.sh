fadb3ccaf4cec45c933ebe642c79ce1c6a6a6bd6
Mihai Maruseac <mihai.maruseac@gmail.com>
2016-08-16 13:34:20 -0400

    No more phase_transition script

--- tools/run_phase_trans.sh
+++ tools/run_phase_trans.sh
@@ -9,6 +9,6 @@ outdir=icde_2016_outputs/private/phase_tran/10rules
 
 for seed in ${seeds}; do
     (./dph p inputs/data/datasets/${datafile}.dat\
-        0.5 0.01 0.5 ${rmax} 10 ${ni} ${seed} | tee\
+        0.5 0.01 0.5 ${rmax} 10 ${ni} ${seed} >\
         ${outdir}/${datafile}_ni${ni}_rmax${rmax}_seed${seed}.txt)&
 done
