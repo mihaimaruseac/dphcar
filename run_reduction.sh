712dd86ac49b6c24221f8d7adfd358332092177a
Mihai Maruseac <mihai.maruseac@gmail.com>
2016-08-17 09:13:31 -0400

    Changes to the run script

--- run_reduction.sh
+++ run_reduction.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-datafiles="mushroom retail pumsb_star"
-rmaxes="3 5"
-nis="10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200"
-rfs="10 100 1000 10000"
+#datafiles="mushroom retail pumsb_star"
+#datafiles="pumsb_star" # TODO: need to run retail 5
+datafiles="retail"
+#rmaxes="3 5"
+rmaxes="5"
+nis=`seq 10 10 2500`
+rfs="10 20 50 80 100 200 500 1000 5000 10000"
 seeds="42 132 243 314 4562"
 
 for df in ${datafiles}; do
