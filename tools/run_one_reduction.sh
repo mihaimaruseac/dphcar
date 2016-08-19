55fc59fb946e9cd4f8ece53224aed1b2fc9b7c8e
Mihai Maruseac <mihai.maruseac@gmail.com>
2016-08-19 10:14:19 -0400

    Update run scripts

--- tools/run_one_reduction.sh
+++ tools/run_one_reduction.sh
@@ -5,7 +5,7 @@ rmax=$2
 ni=$3
 rf=$4
 seed=$5
-outdir=icde_2016_outputs/private/reduction_factor/10rules
+outdir=icde_2016_outputs/private/reduction_factor_2/10rules
 
 ./dph p inputs/data/datasets/${datafile}.dat 0.5 0.01 0.5 ${rmax} 10\
     ${ni} ${rf} ${seed} >\
--- tools/run_reduction.sh
+++ tools/run_reduction.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-datafiles="mushroom retail pumsb_star"
 rmaxes="3 5"
-nis=`seq 10 10 2500`
-rfs="10 20 50 80 100 200 500 1000 5000 10000"
+rfs="10 20 30 40 50 60 70 80 90 100 125 150 175 200"
 seeds="42 132 243 314 4562"
 
-for df in ${datafiles}; do
+run_on_db () {
+    df=$1
+    maxni=$2
+
     for rmax in ${rmaxes}; do
-        for ni in ${nis}; do
+        for ni in `seq 10 10 ${maxni}`; do
             for rf in ${rfs}; do
                 for seed in ${seeds}; do
                     echo ${df} ${rmax} ${ni} ${rf} ${seed}
                 done
             done
         done
-    done
-done | xargs -n5 -P7 ./run_one_reduction.sh
+    done | xargs -n5 -P7 ./run_one_reduction.sh
+}
+
+run_on_db mushroom 120
+run_on_db retail 1000
+run_on_db pumsb_star 600
