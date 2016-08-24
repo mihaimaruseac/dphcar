831405d274f7a3cbb097fb09ce317f976f16c9fe
Mihai Maruseac <mihai.maruseac@gmail.com>
2016-08-24 10:02:42 -0400

    Move comment to the code it comments on

--- rs.h
+++ rs.h
@@ -13,6 +13,7 @@
 struct reservoir;
 struct drand48_data;
 
+/* Notice one extra parameter when tracing the reservoir */
 struct reservoir *init_reservoir(size_t sz,
 #if PRINT_RS_TRACE
 		void (*print_fun)(void *it),
@@ -21,7 +22,6 @@ struct reservoir *init_reservoir(size_t sz,
 		void (*free_fun)(void *it));
 void free_reservoir(struct reservoir *r);
 
-/* Notice one extra parameter when tracing the reservoir */
 void add_to_reservoir(struct reservoir *r, const void *it, size_t sz,
 		double w, struct drand48_data *randbuffer);
 void add_to_reservoir_log(struct reservoir *r, const void *it, size_t sz,
