5a39ee07c5e968c9a92efd3c5b557a59b92903b8
Mihai Maruseac <mihai.maruseac@gmail.com>
2016-09-13 13:57:15 -0400

    Semantic renames

--- itstree.c
+++ itstree.c
@@ -3,20 +3,20 @@
 
 #include "itstree.h"
 
-struct node_info {
+struct children_info {
 	int item;
 	struct itstree_node *iptr;
 };
 
 struct itstree_node {
-	struct node_info *data;
+	struct children_info *children;
 	size_t sp;
 	size_t sz;
 };
 
 static int cmp(const void *a, const void *b)
 {
-	const struct node_info *pa = a, *pb = b;
+	const struct children_info *pa = a, *pb = b;
 	return pa->item - pb->item;
 }
 
@@ -25,7 +25,7 @@ struct itstree_node *init_empty_itstree()
 {
 	struct itstree_node *ret = calloc(1, sizeof(*ret));
 	ret->sp = INITIALSZ;
-	ret->data = calloc(ret->sp, sizeof(ret->data[0]));
+	ret->children = calloc(ret->sp, sizeof(ret->children[0]));
 	return ret;
 }
 #undef INITIALSZ
@@ -33,25 +33,25 @@ struct itstree_node *init_empty_itstree()
 #define FILLFACTOR 2
 void record_new_rule(struct itstree_node *itst, const int *cf, size_t sz)
 {
-	struct node_info k, *p;
+	struct children_info k, *p;
 
 	if (!sz)
 		return;
 
 	k.item = cf[0];
-	p = bsearch(&k, itst->data, itst->sz, sizeof(k), cmp);
+	p = bsearch(&k, itst->children, itst->sz, sizeof(k), cmp);
 
 	if (!p) {
 		if (itst->sz == itst->sp) {
 			itst->sp *= FILLFACTOR;
-			itst->data = realloc(itst->data,
-					itst->sp * sizeof(itst->data[0]));
+			itst->children = realloc(itst->children,
+					itst->sp * sizeof(itst->children[0]));
 		}
-		itst->data[itst->sz].item = cf[0];
-		itst->data[itst->sz].iptr = init_empty_itstree();
+		itst->children[itst->sz].item = cf[0];
+		itst->children[itst->sz].iptr = init_empty_itstree();
 		itst->sz++;
-		qsort(itst->data, itst->sz, sizeof(k), cmp);
-		p = bsearch(&k, itst->data, itst->sz, sizeof(k), cmp);
+		qsort(itst->children, itst->sz, sizeof(k), cmp);
+		p = bsearch(&k, itst->children, itst->sz, sizeof(k), cmp);
 	}
 
 	record_new_rule(p->iptr, cf+1, sz-1);
@@ -60,13 +60,13 @@ void record_new_rule(struct itstree_node *itst, const int *cf, size_t sz)
 
 int search_rule(const struct itstree_node *itst, const int *cf, size_t sz)
 {
-	struct node_info k, *p;
+	struct children_info k, *p;
 
 	if (!sz)
 		return 1;
 
 	k.item = cf[0];
-	p = bsearch(&k, itst->data, itst->sz, sizeof(k), cmp);
+	p = bsearch(&k, itst->children, itst->sz, sizeof(k), cmp);
 
 	if (!p)
 		return 0;
@@ -79,7 +79,7 @@ void free_itstree(struct itstree_node *itst)
 	size_t i;
 
 	for (i = 0; i < itst->sz; i++)
-		free_itstree(itst->data[i].iptr);
-	free(itst->data);
+		free_itstree(itst->children[i].iptr);
+	free(itst->children);
 	free(itst);
 }
